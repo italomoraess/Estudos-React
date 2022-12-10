3d11fc62c390f610fd143c1cabc32e93702f123a
italomoraess <italomoraes.sth@gmail.com>
2022-12-09 23:18:03 -0300

    Praticing React

--- estudos/src/App.tsx
+++ estudos/src/App.tsx
@@ -1,21 +1,30 @@
 import './App.css';
-import { Button } from './components/Button';
-import { Contador } from './components/Contador';
-import { Form } from './components/Form';
-import { Loading } from './components/Loading';
+import styled from 'styled-components';
+// import { Button } from './components/Button';
+// import { Contador } from './components/Contador';
+// import { Form } from './components/Form';
+// import { Loading } from './components/Loading';
+
+const Test = styled.div`
+  backgound-color: blue;
+  color: white
+`;
 
 function App() {
 
-  const actionButton = (texto: string) => {
-    alert("Click No APP: " + texto)
-  }
+  // const actionButton = (texto: string) => {
+  //   alert("Click No APP: " + texto)
+  // }
 
   return (
     <div>
       {/* <Form /> */}
       {/* <Button text="Clicar" clickFn={actionButton}/> */}
       {/* <Contador title=''/> */}
-      <Loading />
+      {/* <Loading /> */}
+      <Test>
+        <button className='button'>Clique aqui</button>
+      </Test>
     </div>
   );
 }
